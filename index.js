77bc2ee7dd46a5469f5d1b1bf0365413894124ba
waghmode02 <waghmodenagesh84@gmail.com>
2024-06-22 16:37:04 +0530

    changes in index.js file

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
-const express = require("express");
 const dotenv = require("dotenv");
+dotenv.config({ path: ".env" });
+
+const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const userRoute = require("./route/userRoute");
-dotenv.config({path:".env"});
+
 const app = express();
 const URL = process.env.URL;
 const PORT = process.env.PORT || 3000;
 
+
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
+
 mongoose.connect(URL)
-    .then(() => {
-        console.log("DB Connected successfully");
-        app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`);
-        });
-    })
-    .catch(error => {
-        console.log("Error connecting to MongoDB:", error);
+  .then(() => {
+    console.log("DB Connected successfully");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
+  })
+  .catch(error => {
+    console.log("Error connecting to MongoDB:", error);
+  });
+
 app.use("/api/users", userRoute);
